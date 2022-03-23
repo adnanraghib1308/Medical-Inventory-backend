6df0942e151c73378510789ee91cd1329ab74c72
Adnan Raghib <adnanraghib@192.168.1.13>
2022-03-23 23:43:54 +0530

    final wrapup code

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,14 +1,13 @@
 const jwt = require("jsonwebtoken");
-const config = require("../config");
 
 const generateJwt = (user) => {
-  return jwt.sign(user.toJSON(), config.jwtSecret, {
+  return jwt.sign(user.toJSON(), "sdfasdfa", {
     expiresIn: 31556952, // 24 hours
   });
 };
 
 const validateJwt = (token) => {
-  return jwt.verify(token, config.jwtSecret);
+  return jwt.verify(token, "sdfasdfa");
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const config = require('./config');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
